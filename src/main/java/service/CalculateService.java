fd40e5691aeeb276a250b0303e4e346ce431482a
seonghoo1217 <seonghoo1217@naver.com>
2023-09-26 23:23:26 +0900

    feat :input value validation separate by value type

--- src/main/java/service/CalculateService.java
+++ src/main/java/service/CalculateService.java
@@ -2,17 +2,26 @@ package service;
 
 import validate.InputValidate;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CalculateService {
+
+    public static int result=0;
+
+    public static Queue<String> queue=new LinkedList<>();
     public InputValidate inputValidate=new InputValidate();
+
     public void inputValueValidateForm(String values){
         for (String value : values.split(" ")){
-            if (inputValidate.validateEmptyOrSpaceValue(value)) throw new IllegalArgumentException("Input value is empty");
+            if (inputValidate.validateValue(value))
         }
     }
 
-    public void calculate(String value){
-        if (inputValidate.validateValueIsNumberType(value)) {
+    private void operationRegister(String value){
+    }
 
-        }
+    public void calculate(String value){
+
     }
 }
--- src/main/java/validate/InputValidate.java
+++ src/main/java/validate/InputValidate.java
@@ -3,10 +3,31 @@ package validate;
 public class InputValidate {
 
     public boolean validateEmptyOrSpaceValue(String value){
-        return value.matches("^\\s*$");
+        if (value.matches("^\\s*$")) throw new IllegalArgumentException("Input Value is Empty");
+        return true;
     }
 
     public boolean validateValueIsNumberType(String value){
-        return !value.matches(".*[^0-9 ].*");
+        return value.matches(".*[^0-9 ].*");
+    }
+
+    public boolean validateValueIsOperationType(String value){
+        return !("+-*/".contains(value) && value.length() == 1);
+    }
+
+    public boolean validateValueType(String value){
+        if (validateValueIsNumberType(value)||validateValueIsOperationType(value)) {
+            throw new IllegalArgumentException("Input Value is Not OP or Numeric Type");
+        }
+        return true;
+    }
+
+    public boolean validateValue(String value){
+        if (validateEmptyOrSpaceValue(value)){
+            if (validateValueType(value)){
+                return true;
+            }
+        }
+        return false;
     }
 }
